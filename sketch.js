3eec58fd9b82af5fc82bed8fee2717ae509f1c3b
Eduardo Salermo <duduscotta@gmail.com>
2026-09-01 16:49:01 -0300

    inclusao do trex

---
+++ sketch.js
@@ -0,0 +1,17 @@
+let trex;
+let trexRunning;
+
+function preload(){
+    trexRunning = loadAnimation("trex1.png", "trex3.png","trex4.png" );
+}
+
+function setup(){
+    createCanvas(600,200);
+    trex = createSprite(50,160,20,50);
+    trex.addAnimation("running", trexRunning);
+}
+
+function draw(){
+    background("white");
+    drawSprites();
+}
